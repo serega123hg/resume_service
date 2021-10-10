c34f0f3a47bd75f010dcbe18ef5ec6afa7b26241
Sergey K <potsov90@mail.ru>
2021-10-10 16:25:55 +0300

    added datamodels

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import City, Language, Vacancy, Error, Url
+#from .models import City, Language, Vacancy, Error, Url
+
+admin.site.register(City)
+admin.site.register(Language)
+admin.site.register(Vacancy)
+admin.site.register(Error)
+admin.site.register(Url)
